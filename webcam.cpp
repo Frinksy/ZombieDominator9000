9782b81c0ed9cf933110125356a1fec8108c213e
Rebecca Wardle <mattydwardle@gmail.com>
2019-11-18 13:12:36 +0000

    Formatted code to be indented at the same level

--- webcam.cpp
+++ webcam.cpp
@@ -20,19 +20,15 @@ int main(int argc, char** argv)
     for(;;)
     {
         i++;
-          Mat frame;
-	  cap >> frame;
+        Mat frame;
+        cap >> frame;
         Mat greenFrame;
-          greenSelector(&frame, &greenFrame);
+        greenSelector(&frame, &greenFrame);
 
-          if( frame.empty() ) break; // end of video stream
-          if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
+        if( frame.empty() ) break; // end of video stream
+        if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
         greenBoxer(&greenFrame, &frame);
         imshow("this is you, smile! :)", frame);
     }
-    
-    
-    
-    
     return 0;
 }
