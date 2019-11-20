1473a5a3c0f05104d8ace307634099f3e0b6f972
Adam <akb561@york.ac.uk>
2019-11-20 23:48:46 +0000

    added rectBoxer to cluster green parts together

--- greenDetection.cpp
+++ greenDetection.cpp
@@ -1,6 +1,8 @@
 #include "greenDetection.hpp"
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/photo/cuda.hpp>
+#include <opencv2/core/ocl.hpp>
 #include <iostream>
 using namespace cv;
 
@@ -8,7 +10,7 @@ using namespace cv;
 void greenSelector (Mat *inputFrame, Mat *outputFrame)
 {
 
-    std::vector<int> lows = {60, 100, 80};
+    std::vector<int> lows = {70, 90, 50};
     std::vector<int> highs = {130, 255, 255};
     Mat equalizedFrame;
     Mat convertedFrame;
@@ -18,9 +20,43 @@ void greenSelector (Mat *inputFrame, Mat *outputFrame)
 
     // Threshold for green
     inRange(convertedFrame, lows, highs, *outputFrame);
-
+    
     // Finishing touches
     medianBlur(*outputFrame, *outputFrame, 7);
+
+}
+
+Rect rectBoxer (std::vector<Rect> rects)
+{
+    /*
+    int minX = rects[0].x;
+    int minY = rects[0].y;
+    int maxX = rects[0].width + minX;
+    int maxY = rects[0].height + minY;
+    */
+    int minX  = 276447231 , minY = 276447231, maxX = 0, maxY = 0;
+
+
+    for (int i = 0; i < rects.size(); i++)
+    {   
+        if (rects[i].width * rects[i].height > 50) // TODO : tune this check to better suit noise elimination
+        {
+            if (rects[i].x < minX){
+                minX = rects[i].x;
+            }
+            if (rects[i].y < minY){
+                minY = rects[i].y;
+            }
+            if (rects[i].width + rects[i].x > maxX){
+                maxX = rects[i].width + rects[i].x;
+            }
+            if (rects[i].height + rects[i].y > maxY){
+                maxY = rects[i].height + rects[i].y;
+            }
+        }
+    }
+    std::cout << Rect(minX, minY, maxX-minX, maxY-minY) << std::endl;
+    return Rect(minX, minY, maxX-minX, maxY-minY);
 }
 
 void greenBoxer(Mat *inputFrame, Mat *outputFrame)
@@ -33,13 +69,15 @@ void greenBoxer(Mat *inputFrame, Mat *outputFrame)
 
     if (contours.size())
     {
-
+        std::vector <Rect> rects;
         for (int i = 0; i < contours.size(); i++)
         {
-            Rect r = boundingRect(contours[i]);
-            std::cout << r << std::endl;
-            rectangle(*outputFrame, r, {255,0,0}, 10);
+            rects.push_back( boundingRect(contours[i]) );
+            //rectangle(*outputFrame, boundingRect(contours[i]), {0,0,255}, 10);
+            
         }
-
-    }
+        Rect outerRect  = rectBoxer(rects);
+        //std::cout << outerRect << std::endl;
+        rectangle(*outputFrame, outerRect, {255,0,0}, 2);
+    }  
 }
--- greenDetection.hpp
+++ greenDetection.hpp
@@ -3,9 +3,11 @@
 #include "opencv2/opencv.hpp"
 #include <opencv2/videoio.hpp>
 #include <opencv2/videoio.hpp>
-
+#include <vector>
 
 void greenSelector(cv::Mat *inputFrame, cv::Mat *outputFrame);
+
+cv::Rect rectBoxer(std::vector<cv::Rect> rects);
 
 void greenBoxer(cv::Mat *inputFrame, cv::Mat *outputFrame);
 
