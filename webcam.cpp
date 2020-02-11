423f74ac768b2142b883b06c4c6ac663c3341a46
Rebecca Wardle <mattydwardle@gmail.com>
2020-02-11 21:29:21 +0000

    Got webcam working on pi (default for webcam is -1)

--- webcam.cpp
+++ webcam.cpp
@@ -1,31 +1,38 @@
-#include "opencv2/opencv.hpp"
-#include <opencv2/videoio.hpp>
 #include <vector>
-#include "greenDetection.hpp"
-#include <opencv2/core/ocl.hpp>
 #include <iostream>
+#include <string>
+
+#include <opencv2/core.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/videoio.hpp>
+
+#include "greenDetection.hpp"
 using namespace cv;
+using namespace std;
 
 
 int main(int argc, char** argv)
 {
     VideoCapture cap;
-    
+
     //cap.set(CAP_PROP_FRAME_WIDTH, 240);
     //cap.set(CAP_PROP_FRAME_HEIGHT, 320);
     if (argc == 1) 
     {
         if(!cap.open(0))
         {
-            std::cout << "No camera found on channel 0\n";
-            return 0;
+            cout << "No camera found on channel 0\n";
         }
     }
     else 
     {
-        if (!cap.open(argv[1]))
+        int deviceID = stoi(argv[1]);
+        int apiID = CAP_ANY;
+        cout << deviceID << endl;
+        cap.open(deviceID + apiID);
+        if (!cap.isOpened())
         {
-            std::cout << "No camera found on " << argv[1] << " channel\n";
+            cout << "No camera found on " << argv[1] << " channel\n";
             return 0;
         }
     }
@@ -35,21 +42,31 @@ int main(int argc, char** argv)
     {
         Mat frame;
 	    //cap >> frame;
-        cap.retrieve(frame);
+        cap.read(frame);
         if( frame.empty() )
         {
-            std::cout << "Camera frame empty\n";
+            cout << "Frame empty\n";
             break;
         }
         if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
-        Mat greenFrame;
+        
+        Mat greenFrame, cleanFrame;
+        //cleanFrame = frame.clone();
+
         greenSelector(&frame, &greenFrame);
         greenBoxer(&greenFrame, &frame);
         
+        char greenWindow[] = "Green";
+        char turretWindow[] = "Turret Vision";
+        namedWindow(greenWindow);
+        namedWindow(turretWindow);
 
-        imshow("GREEN", greenFrame);
+        moveWindow(greenWindow, 0, 0);
+        moveWindow(turretWindow, frame.cols+3, 36);
 
-        imshow("Turret VISION", frame);
+        //imshow("CleanFrame", cleanFrame);
+        imshow(greenWindow, greenFrame);
+        imshow(turretWindow, frame);
     }
     return 0;
 }
